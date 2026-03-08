121ebbbafb7eac5664b02e059799539ffe3ef697
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-09 00:22:31 +0800

    Implement provider for setting

---
+++ lib/settings_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsProvider extends ChangeNotifier {
+  bool _isDarkMode = false;
+  Locale _locale = const Locale('en');
+
+  bool get isDarkMode => _isDarkMode;
+  Locale get locale => _locale;
+
+  SettingsProvider() {
+    _loadSettings();
+  }
+
+  Future<void> _loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    String? languageCode = prefs.getString('languageCode');
+    if (languageCode != null) {
+      _locale = Locale(languageCode);
+    }
+    notifyListeners();
+  }
+
+  Future<void> toggleDarkMode(bool value) async {
+    _isDarkMode = value;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', value);
+    notifyListeners();
+  }
+
+  Future<void> setLocale(Locale locale) async {
+    _locale = locale;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('languageCode', locale.languageCode);
+    notifyListeners();
+  }
+}
